d4973895aa887418f76f600fc3a3a54bbfd9552b
habibidaffaa <habibidaffa123@gmail.com>
2023-09-23 05:09:29 +0700

    add main

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:manhatan_project/presentation/pages/splash_screen.dart';
+import 'package:provider/provider.dart';
+import 'presentation/providers/button_providers.dart';
+
+void main() {
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => ButtonProviders(),
+      child: const MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SplashScreen(),
+    );
+  }
+}
